822ab19419066674a874ac15479e20e779aae1ad
timfedo <timfedo@gmail.com>
2020-07-31 17:51:36 +0300

    Added music control actions

--- app/src/main/java/com/kieronquinn/app/taptap/models/TapAction.kt
+++ app/src/main/java/com/kieronquinn/app/taptap/models/TapAction.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 import com.kieronquinn.app.taptap.R
 import com.kieronquinn.app.taptap.columbus.actions.LaunchApp
 import com.kieronquinn.app.taptap.columbus.actions.LaunchAssistant
+import com.kieronquinn.app.taptap.columbus.actions.MusicAction
 import com.kieronquinn.app.taptap.columbus.actions.TaskerEvent
 import com.kieronquinn.app.taptap.utils.AccessibilityServiceGlobalAction
 import com.kieronquinn.app.taptap.utils.LaunchCameraLocal
@@ -22,7 +23,10 @@ enum class TapAction(val clazz: Class<*>, @StringRes val nameRes: Int, @StringRe
     LOCK_SCREEN(AccessibilityServiceGlobalAction::class.java, R.string.action_lock_screen, R.string.action_lock_screen_desc, R.drawable.ic_power_state, minApi(28), true),
     FLASHLIGHT(AccessibilityServiceGlobalAction::class.java, R.string.action_flashlight, R.string.action_flashlight_desc, R.drawable.ic_action_category_utilities, true, true, dataType = ActionDataTypes.CAMERA_PERMISSION),
     LAUNCH_APP(LaunchApp::class.java, R.string.action_launch_app, R.string.action_launch_app_desc, R.drawable.ic_action_category_launch, true, true, R.string.action_launch_app_desc_formattable, ActionDataTypes.PACKAGE_NAME),
-    TASKER_EVENT(TaskerEvent::class.java, R.string.action_tasker_event, R.string.action_tasker_event_desc, R.drawable.ic_launch_assistant, true, true)
+    TASKER_EVENT(TaskerEvent::class.java, R.string.action_tasker_event, R.string.action_tasker_event_desc, R.drawable.ic_launch_assistant, true, true),
+    TOGGLE_PAUSE(MusicAction::class.java, R.string.action_toggle_pause, R.string.action_toggle_pause_desc, R.drawable.ic_action_toggle_pause, true, true),
+    PREVIOUS(MusicAction::class.java, R.string.action_previous, R.string.action_previous_desc, R.drawable.ic_action_previous, true, true),
+    NEXT(MusicAction::class.java, R.string.action_next, R.string.action_next_desc, R.drawable.ic_action_next, true, true)
 }
 
 enum class ActionDataTypes {
